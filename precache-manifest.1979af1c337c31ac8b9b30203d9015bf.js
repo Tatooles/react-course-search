54e85d01eb0d1c60c726a3e274b3c1b8bc012dc3
Kevin Tatooles <tatooles@wisc.edu>
2022-01-13 17:22:34 -0600

    Updates

---
+++ precache-manifest.1979af1c337c31ac8b9b30203d9015bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504450f7ee6ba7c1f91a3171d253417a",
+    "url": "/react-course-search/index.html"
+  },
+  {
+    "revision": "58ac917f26c3efb930c9",
+    "url": "/react-course-search/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5caeaae02598237feb23",
+    "url": "/react-course-search/static/js/2.ea490775.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/react-course-search/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "58ac917f26c3efb930c9",
+    "url": "/react-course-search/static/js/main.63dc14f7.chunk.js"
+  },
+  {
+    "revision": "ac9fea6db7a15b962ae8",
+    "url": "/react-course-search/static/js/runtime~main.5ae92447.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-course-search/static/media/logo.06e73328.svg"
+  }
+]);
